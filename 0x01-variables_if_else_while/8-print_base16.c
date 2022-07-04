dc2fb3d49a701c99479159b7c2f7b50a4063d6cc
Enoch Aik <enoch_aik@icloud.com>
2022-07-04 15:35:54 -0700

    fixed indent in task 8

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -9,11 +9,12 @@
  **/
 int main(void)
 {
-        /**
-         *
-         * hexadecimal
-         * 0 1 2 3 4 5 6 7 8 9 a c d e f
-         */
+	/**
+	*
+	*
+	* hexadecimal
+	* 0 1 2 3 4 5 6 7 8 9 a c d e f
+	*/
 	char ch;
 	int n;
 
